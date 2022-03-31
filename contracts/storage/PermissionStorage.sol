8e7ecbbee8d9affb766d647ca700dacd858d6f02
Papa Smurf <chris@violet.co>
2022-03-31 23:31:24 +0100

    removed _onlyOwner from permissioningStorage

--- contracts/storage/PermissionStorage.sol
+++ contracts/storage/PermissionStorage.sol
@@ -27,21 +27,4 @@ library Permissions {
             roleStorage.slot := position
         }
     }
-    
-    function _onlyOwner() internal view {
-        RoleState storage state = _getStorage();
-        // PROBLEM:
-        // msg.sender in Extendable contracts will often refer to itself
-        // Use cases that require checking original transaction sender should use tx.origin
-        // Otherwise, check msg.sender with extreme prejudice as calls can come from various unknown sources
-        // This does render some use cases impossible if performing nested delegatecalls that require knowledge of the caller at a midpoint:
-        // tx.origin -> contract A -> contract B -> contract A -> contract A (requires last external caller)
-        //     ^                                        ^
-        //     | only these two data points are checked |
-        // Can be worked around by implementing logic that passes down addresses but then code security of this becomes paramount.
-        // A malicious contract B can circumvent any checks that are required through an injection attack.
-        
-        // In order to check msg.sender properly here, we ensure that the caller is the current contract and then check tx.origin
-        require(state.owner == msg.sender || (state.owner == tx.origin && msg.sender == address(this)), "unauthorised"); // Exercise EXTREME CAUTION when using this pattern elsewhere
-    }
 }
