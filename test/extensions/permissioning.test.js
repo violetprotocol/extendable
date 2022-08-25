42bcb7ceb33aa339077f32ec0c1b43f4a758e98c
Papa Smurf <chris@violet.co>
2022-08-25 14:45:05 +0100

    test: simplified permissioning test

--- test/extensions/permissioning.test.js
+++ test/extensions/permissioning.test.js
@@ -11,6 +11,7 @@ describe("PermissioningLogic", function () {
     let logic;
     let caller;
     const NULL_ADDRESS = "0x000000000000000000000000000000000000dEaD";
+    let tx;
 
     beforeEach("deploy new", async function () {
         [account, account2] = await ethers.getSigners();
@@ -24,7 +25,31 @@ describe("PermissioningLogic", function () {
         await caller.deployed();
     });
 
+    describe("ERC165 compatibility", async function () {
+        it("should register interface id during constructor correctly", async function () {
+            const extensionAsEIP165 = await getExtendedContractWithInterface(logic.address, "ERC165Logic");
+            expect(await extensionAsEIP165.callStatic.supportsInterface(PERMISSIONING.INTERFACE)).to.be.true;
+        });
+
+        it("should return implemented interfaces correctly", async function () {
+            expect(await logic.callStatic.getInterface()).to.deep.equal([[PERMISSIONING.INTERFACE, PERMISSIONING.SELECTORS]]);
+        });
+
+        it("should return solidity interface correctly", async function () {
+            expect(await logic.callStatic.getSolidityInterface()).to.equal("".concat(
+                "function init() external;\n",
+                "function updateOwner(address newOwner) external;\n",
+                "function renounceOwnership() external;\n",
+                "function getOwner() external view returns(address);\n"
+            ));
+        });
+    });
+
     describe("direct", function () {
+        beforeEach("call init", async function () {
+            tx = await expect(caller.init()).to.not.be.reverted;
+        });
+
         it("initialise owner should succeed", async function () {
             await expect(logic.init()).to.emit(logic, "OwnerUpdated").withArgs(account.address);
             expect(await logic.callStatic.getOwner()).to.equal(account.address);
@@ -72,61 +97,41 @@ describe("PermissioningLogic", function () {
             await expect(logic.renounceOwnership()).to.emit(logic, "OwnerUpdated").withArgs(NULL_ADDRESS);
             expect(await logic.callStatic.getOwner()).to.equal(NULL_ADDRESS);
         });
-
-        it("should register interface id during constructor correctly", async function () {
-            const extensionAsEIP165 = await getExtendedContractWithInterface(logic.address, "ERC165Logic");
-            expect(await extensionAsEIP165.callStatic.supportsInterface(PERMISSIONING.INTERFACE)).to.be.true;
-        });
-
-        it("should return implemented interfaces correctly", async function () {
-            expect(await logic.callStatic.getInterface()).to.deep.equal([[PERMISSIONING.INTERFACE, PERMISSIONING.SELECTORS]]);
-        });
-
-        it("should return solidity interface correctly", async function () {
-            expect(await logic.callStatic.getSolidityInterface()).to.equal("".concat(
-                "function init() external;\n",
-                "function updateOwner(address newOwner) external;\n",
-                "function renounceOwnership() external;\n",
-                "function getOwner() external view returns(address);\n"
-            ));
-        });
     });
 
     describe("delegated", function () {
+        beforeEach("call init", async function () {
+            tx = await expect(caller.init()).to.not.be.reverted;
+        });
+
         it("initialise owner should succeed", async function () {
-            const tx = await expect(caller.init()).to.not.be.reverted;
             await expectEvent(tx, logic.interface, "OwnerUpdated", { newOwner: account.address });
             expect(await caller.callStatic.getOwner()).to.equal(account.address);
         });
 
         it("initialise already initialised contract should fail", async function () {
-            await expect(caller.init()).to.not.be.reverted;
             await expect(caller.connect(account2).init()).to.be.revertedWith("PermissioningLogic: already initialised");
             expect(await caller.callStatic.getOwner()).to.equal(account.address);
         });
 
         it("update owner should succeed", async function () {
-            await expect(caller.init()).to.not.be.reverted;
-            const tx = await expect(caller.updateOwner(account2.address)).to.not.be.reverted;
+            tx = await expect(caller.updateOwner(account2.address)).to.not.be.reverted;
             await expectEvent(tx, logic.interface, "OwnerUpdated", { newOwner: account2.address });
             expect(await caller.callStatic.getOwner()).to.equal(account2.address);
         });
     
         it("update owner with the zero address should fail", async function () {
-            await expect(caller.init()).to.not.be.reverted;
             await expect(caller.updateOwner(ethers.constants.AddressZero)).to.be.revertedWith("new owner cannot be the zero address");
             expect(await caller.callStatic.getOwner()).to.equal(account.address);
         });
     
         it("update owner when unauthorized should fail", async function () {
-            await expect(caller.init()).to.not.be.reverted;
             await expect(caller.connect(account2).updateOwner(account2.address)).to.be.revertedWith("unauthorised");
             expect(await caller.callStatic.getOwner()).to.equal(account.address);
         });
     
         it("update owner to original owner should succeed", async function () {
-            await expect(caller.init()).to.not.be.reverted;
-            let tx = await expect(caller.updateOwner(account2.address)).to.not.be.reverted;
+            tx = await expect(caller.updateOwner(account2.address)).to.not.be.reverted;
             await expectEvent(tx, logic.interface, "OwnerUpdated", { newOwner: account2.address });
             tx = await expect(caller.connect(account2).updateOwner(account.address)).to.not.be.reverted;
             await expectEvent(tx, logic.interface, "OwnerUpdated", { newOwner: account.address });
@@ -134,14 +139,12 @@ describe("PermissioningLogic", function () {
         });
     
         it("renouncing ownership when unauthorized should fail", async function () {
-            await expect(caller.init()).to.not.be.reverted;
             await expect(caller.connect(account2).renounceOwnership()).to.be.revertedWith("unauthorised");
             expect(await caller.callStatic.getOwner()).to.equal(account.address);
         });
 
         it("renouncing ownership should set owner to the null address", async function () {
-            await expect(caller.init()).to.not.be.reverted;
-            const tx = await expect(caller.renounceOwnership()).to.not.be.reverted;
+            tx = await expect(caller.renounceOwnership()).to.not.be.reverted;
             await expectEvent(tx, logic.interface, "OwnerUpdated", { newOwner: NULL_ADDRESS });
             expect(await caller.callStatic.getOwner()).to.equal(NULL_ADDRESS);
         });
